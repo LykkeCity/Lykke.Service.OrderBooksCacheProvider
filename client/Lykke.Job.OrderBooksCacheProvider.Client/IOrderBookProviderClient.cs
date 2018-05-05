41337d1f519b1459249bd9e006b7df0d26970f27
Alexey Novichikhin <alexey.novichikhin@lykke.com>
2018-05-05 23:31:28 +0200

    fix url in client lib

--- client/Lykke.Job.OrderBooksCacheProvider.Client/IOrderBookProviderClient.cs
+++ client/Lykke.Job.OrderBooksCacheProvider.Client/IOrderBookProviderClient.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Lykke.Job.OrderBooksCacheProvider.Client
 {
@@ -11,67 +7,5 @@ namespace Lykke.Job.OrderBooksCacheProvider.Client
     {
         Task<List<OrderBookRaw>> GetOrderBookRawAsync(string assetPair);
         Task<OrderBook> GetOrderBookAsync(string assetPair);
-    }
-
-    public class OrderBookProviderClient : IOrderBookProviderClient, IDisposable
-    {
-        private readonly string _serviceUrl;
-        private readonly HttpClient _client;
-
-        public OrderBookProviderClient(string serviceUrl)
-        {
-            _serviceUrl = serviceUrl;
-            if (!_serviceUrl.EndsWith("/")) _serviceUrl += "/";
-            _serviceUrl += "OrderBookProvider";
-            _client = new HttpClient();
-        }
-
-        public async Task<List<OrderBookRaw>> GetOrderBookRawAsync(string assetPair)
-        {
-            var responce = await _client.GetAsync(_serviceUrl);
-            var content = await responce.Content.ReadAsStringAsync();
-            if (responce.StatusCode != HttpStatusCode.OK)
-            {
-                throw new Exception($"Error on call to {_serviceUrl}. StatusCode = {responce.StatusCode}. Content = {content}");
-            }
-
-            return JsonConvert.DeserializeObject<List<OrderBookRaw>>(content);
-        }
-
-        public async Task<OrderBook> GetOrderBookAsync(string assetPair)
-        {
-            var raw = await GetOrderBookRawAsync(assetPair);
-
-            var result = new OrderBook()
-            {
-                AssetPair = assetPair,
-                Timestamp = DateTime.UtcNow,
-                Prices = new List<VolumePrice>()
-            };
-
-            if (raw.Count == 0)
-            {
-                result.Timestamp = DateTime.UtcNow;
-            }
-
-            if (raw.Count >= 1)
-            {
-                result.Timestamp = raw[0].Timestamp;
-                result.Prices.AddRange(raw[0].Prices);
-            }
-            
-            if (raw.Count >= 2 && result.Timestamp < raw[1].Timestamp)
-            {
-                result.Timestamp = raw[1].Timestamp;
-                result.Prices.AddRange(raw[1].Prices);
-            }
-
-            return result;
-        }
-
-        public void Dispose()
-        {
-            _client?.Dispose();
-        }
     }
 }
