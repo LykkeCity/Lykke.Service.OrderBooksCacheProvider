2ecde38020034c48eab0e02c87f33e7900ee3ed9
Alexey Novichikhin <alexey.novichikhin@lykke.com>
2018-05-05 21:41:00 +0200

    add bad request if asset pair is empty

--- src/Lykke.Job.OrderBooksCacheProvider/Controllers/OrderBookProviderController.cs
+++ src/Lykke.Job.OrderBooksCacheProvider/Controllers/OrderBookProviderController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Lykke.Common.Api.Contract.Responses;
 using Lykke.Job.OrderBooksCacheProvider.Core.Domain;
 using Lykke.Job.OrderBooksCacheProvider.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Lykke.Job.OrderBooksCacheProvider.Controllers
@@ -24,8 +27,17 @@ namespace Lykke.Job.OrderBooksCacheProvider.Controllers
         [SwaggerOperation("GetOrderBook")]
         [ProducesResponseType(typeof(List<OrderBook>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> GetOrderBook(string assetPair)
         {
+            if (string.IsNullOrEmpty(assetPair))
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    ErrorMessage = "assetPair cannot be empty"
+                });
+            }
+
             try
             {
                 return Ok(await _booksProvider.GetCurrentOrderBooksAsync(assetPair));
